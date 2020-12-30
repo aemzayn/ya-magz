c704ac7d5e958f155023ce98066eba89428aba64
aemzayn <ahmad.muslih22@gmail.com>
2020-12-30 08:57:32 +0700

    deploy to github

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import { Articles, Hero } from '../components/home'
 import Newsletter from '../components/home/newsletter'
 import Footer from '../components/sections/footer'
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
   return (
     <>
       <Head>
@@ -15,4 +16,19 @@ export default function Home() {
       <Footer />
     </>
   )
+}
+
+export async function getServerSideProps(ctx) {
+  const res = await fetch(`${process.env.API_URL}/articles`)
+  const data = await res.json()
+  if (data.errors) {
+    console.error(data.errors)
+    throw new Error('Failed to fetch API')
+  }
+
+  return {
+    props: {
+      data,
+    }, // will be passed to the page component as props
+  }
 }
