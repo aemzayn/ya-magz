486aeea2c808358ab088dff81dacea48c559a92c
aemzayn <ahmad.muslih22@gmail.com>
2021-01-03 16:36:35 +0700

    change import paths in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,14 @@
-import Header from '@/components/sections/header'
+import Header from '../components/sections/header'
 import { ThemeProvider, theme, CSSReset } from '@chakra-ui/react'
-import { GlobalProvider } from '@/context/global'
-import '@/scss/styles.scss'
+import '../scss/styles.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <GlobalProvider>
-      <ThemeProvider theme={theme}>
-        <Header />
-        <CSSReset />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </GlobalProvider>
+    <ThemeProvider theme={theme}>
+      <Header />
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
