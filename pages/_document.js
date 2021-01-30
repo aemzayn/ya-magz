1beb215eb6715b00ce669081aceda5734953166e
aemzayn <ahmad.muslih22@gmail.com>
2021-01-30 11:07:36 +0700

    fix fragment wasnt imported in document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { GA_TRACKING_ID } from '@/lib/gtag.js'
+import { Fragment } from 'react'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
