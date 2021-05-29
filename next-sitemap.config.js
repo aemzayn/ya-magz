eab1db808b54b8c362a088cb310fc4bf999c01c5
aemzayn <ahmad.muslih22@gmail.com>
2021-05-29 20:56:37 +0700

    fix import path in next sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
-const config = require('@/cms/site-settings.json')
+const config = require("./cms/site-settings.json")
 
 module.exports = {
-  siteUrl: config.base_url || 'https://ya-magazine.netlify.app/',
+  siteUrl: config.base_url || "https://ya-magazine.netlify.app/",
   generateRobotsTxt: true,
 }
