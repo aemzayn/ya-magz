df123849fb549be0f2f20bfbc994459f6cf2c673
aemzayn <ahmad.muslih22@gmail.com>
2021-05-31 19:38:13 +0700

    change authors layout

--- src/pages/author/index.js
+++ src/pages/author/index.js
@@ -1,19 +1,26 @@
-import Meta from '@/components/article/meta/meta'
-import PageLayout from '@/components/page-layout'
-import Layout from '@/components/sections/layout'
-import { listAuthor } from '@/lib/authors.js'
-import Icon from '@chakra-ui/icon'
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input'
-import { AspectRatio, Box, Flex, Heading, SimpleGrid } from '@chakra-ui/layout'
-import { useBreakpointValue } from '@chakra-ui/media-query'
-import { chakra } from '@chakra-ui/system'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { FiSearch } from 'react-icons/fi'
+import { useEffect, useState } from "react"
+import Meta from "@/components/article/meta/meta"
+import PageLayout from "@/components/page-layout"
+import Layout from "@/components/sections/layout"
+import { listAuthor } from "@/lib/authors.js"
+import {
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Box,
+  Flex,
+  Heading,
+  Text,
+  VStack,
+  useBreakpointValue,
+} from "@chakra-ui/react"
+import Link from "next/link"
+import { FiSearch } from "react-icons/fi"
 
 export default function Author({ authors }) {
   const [showAuthors, setShowAuthors] = useState(authors)
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("")
 
   useEffect(() => {
     if (search.length > 0) {
@@ -30,10 +37,10 @@ export default function Author({ authors }) {
     )
   }
 
-  const nameTextAlign = useBreakpointValue({ base: 'center', md: 'center' })
+  const nameTextAlign = useBreakpointValue({ base: "center", md: "center" })
 
-  const url = '/author'
-  const title = 'Authors'
+  const url = "/author"
+  const title = "Authors"
 
   return (
     <Layout>
@@ -41,54 +48,54 @@ export default function Author({ authors }) {
         url={url}
         title={title}
         keywords={[
-          'author',
-          'writer',
-          'ya magazine author',
-          'ya magazine writer',
+          "author",
+          "writer",
+          "ya magazine author",
+          "ya magazine writer",
         ]}
-        description='List of Ya! Magazine content writers'
+        description="List of Ya! Magazine content writers"
       />
 
       <PageLayout
-        w={{ base: 'full', md: '80vw' }}
-        justifyContent='flex-start'
-        minH='72vh'
-        mx='auto'
+        w={{ base: "full", md: "80vw" }}
+        justifyContent="flex-start"
+        minH="72vh"
+        mx="auto"
       >
         <Flex
           mb={{ base: 2, md: 4, xl: 8 }}
-          w='full'
-          flexDir={{ base: 'column', md: 'row' }}
-          alignItems='center'
-          justifyContent={{ base: 'space-between' }}
-          pos='relative'
+          w="full"
+          flexDir={{ base: "column", md: "row" }}
+          alignItems="center"
+          justifyContent={{ base: "space-between" }}
+          pos="relative"
         >
           <Box
-            pos={{ base: 'relative', xl: 'absolute' }}
-            left={{ base: 'unset', xl: '50%' }}
-            transform={{ base: 'unset', xl: 'translateX(-50%)' }}
+            pos={{ base: "relative", xl: "absolute" }}
+            left={{ base: "unset", xl: "50%" }}
+            transform={{ base: "unset", xl: "translateX(-50%)" }}
           >
-            <Heading>{search.length > 0 ? search : 'Authors'}</Heading>
+            <Heading>{search.length > 0 ? search : "Authors"}</Heading>
           </Box>
-          <Box ml={{ base: 'unset', xl: 'auto' }} mt={{ base: 2, md: 'unset' }}>
+          <Box ml={{ base: "unset", xl: "auto" }} mt={{ base: 2, md: "unset" }}>
             <InputGroup>
               <InputLeftElement
                 children={<Icon as={FiSearch} />}
-                pointerEvents='none'
-                color='gray.300'
+                pointerEvents="none"
+                color="gray.300"
               />
               <Input
-                type='text'
+                type="text"
                 maxLength={15}
                 value={search}
                 onChange={e => setSearch(e.target.value)}
-                borderRadius='0'
-                borderColor='gray.300'
+                borderRadius="0"
+                borderColor="gray.300"
                 px={{ base: 2 }}
-                focusBorderColor='brand.main'
-                placeholder='Search author'
+                focusBorderColor="brand.main"
+                placeholder="Search author"
                 _placeholder={{
-                  color: 'gray.300',
+                  color: "gray.300",
                 }}
                 _focus={{
                   pr: 2,
@@ -97,35 +104,21 @@ export default function Author({ authors }) {
             </InputGroup>
           </Box>
         </Flex>
-        <SimpleGrid
-          w={{ base: 'full', md: '80vw' }}
-          columns={{ base: 2, md: 4, lg: 5 }}
-        >
+        <VStack mt={{ base: 4 }} w="full" spacing={{ base: 4 }}>
           {Array.isArray(showAuthors) &&
             showAuthors.map((author, id) => (
-              <AspectRatio
-                ratio={{ base: 2 / 1, md: 1 / 1 }}
-                key={id}
-                flex={1}
-                bgColor='white'
-                borderRadius='sm'
-                cursor='pointer'
-                _hover={{
-                  bgColor: 'brand.light',
-                }}
-              >
-                <Link href={`/author/${author.slug}`}>
-                  <chakra.a
-                    fontSize={{ base: 'sm', md: 'md' }}
-                    w='full'
-                    textAlign={nameTextAlign}
-                  >
-                    {author.name}
-                  </chakra.a>
-                </Link>
-              </AspectRatio>
+              <Link href={`/author/${author.slug}`}>
+                <Text
+                  cursor="pointer"
+                  as="a"
+                  key={id}
+                  _hover={{ color: "gray.500" }}
+                >
+                  {author.name}
+                </Text>
+              </Link>
             ))}
-        </SimpleGrid>
+        </VStack>
       </PageLayout>
     </Layout>
   )
