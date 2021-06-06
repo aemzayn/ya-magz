aa922d278d5768ad171b320746395b51b99c8b1f
aemzayn <ahmad.muslih22@gmail.com>
2021-06-06 10:31:57 +0700

    update manifest.json

--- src/components/sections/layout.js
+++ src/components/sections/layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="manifest" href="/meta/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
