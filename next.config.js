2b61a5315a8193cd7d770cdeedd0b37ec1018f1e
aemzayn <ahmad.muslih22@gmail.com>
2021-06-30 08:22:36 +0700

    add image optimization plugins

--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
+const withPlugins = require("next-compose-plugins")
 const withPWA = require("next-pwa")
+const optimizedImages = require("next-optimized-images")
 
 const workboxConfig = require("./wb.config")
 
-module.exports = withPWA({
-  webpack: config => {
-    config.module.rules.push(
-      ...[
-        {
-          test: /\.yml$/,
-          type: "json",
-          use: "yaml-loader",
-        },
-      ]
-    )
-
-    return config
-  },
-  eslint: {
-    // Warning: Dangerously allow production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+const nextConfig = {
   images: {
     deviceSizes: [420, 768, 1024, 1200],
     iconSizes: [],
@@ -29,4 +13,36 @@ module.exports = withPWA({
     loader: "default",
   },
   pwa: workboxConfig,
-})
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        webpack(config) {
+          config.module.rules.push(
+            ...[
+              {
+                test: /\.yml$/,
+                type: "json",
+                use: "yaml-loader",
+              },
+            ]
+          )
+          return config
+        },
+      },
+    ],
+    [
+      optimizedImages,
+      {
+        handleImages: ["jpeg", "png", "svg"],
+      },
+    ],
+  ],
+  nextConfig
+)
