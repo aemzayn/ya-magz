8b17a25cb84d57b650fb4a60295f419aa84eff64
aemzayn <ahmad.muslih22@gmail.com>
2021-07-03 09:37:48 +0700

    move posts

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,7 +3,7 @@ import useSWR from "swr"
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-export default function useFetch(endpoint) {
+export default function fetch(endpoint) {
   const { data, error } = useSWR(endpoint, fetcher)
   return {
     data,
