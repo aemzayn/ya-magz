43fba3e41b6684620ce793c9c1a6516123b8147e
Ahmad Muslih Zain <44734533+aemzayn@users.noreply.github.com>
2021-12-22 15:50:14 +0300

    add google ads script

--- src/components/layout/container.js
+++ src/components/layout/container.js
@@ -1,10 +1,17 @@
+import Head from "next/head"
+import Script from "next/script"
 import { Box } from "@chakra-ui/react"
 
 const Container = ({ children }) => {
   return (
-    <Box maxW="1600px" mx="auto" boxShadow="xl">
-      {children}
-    </Box>
+    <>
+      <Head>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8695710654350407" crossorigin="anonymous" />
+      </Head>
+      <Box maxW="1600px" mx="auto" boxShadow="xl">
+        {children}
+      </Box>
+    </>
   )
 }
 
