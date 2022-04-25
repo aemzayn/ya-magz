403c20739a9fe8b98a1c5f76a0bbe374714fd1e2
Ahmad Muslih Zain <ahmad.muslih@gmail.com>
2022-04-26 00:26:25 +0300

    trigger redeploy

--- src/components/sections/UnderConstructionPage.js
+++ src/components/sections/UnderConstructionPage.js
@@ -1,6 +1,6 @@
+import React from "react"
 import { InfoIcon } from "@chakra-ui/icons"
 import { Box, Flex, Heading } from "@chakra-ui/react"
-import React from "react"
 
 export default function UnderConstructionPage() {
   return (
